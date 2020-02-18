427aa0c68fe0705ca0e285c0a933adaf5f06530a
Bartek <bart.kazm@gmail.com>
2020-02-18 01:07:12 +0100

    Updates

---
+++ precache-manifest.4f0f05ad412a13802f59fdd590a67769.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c24347e77dac488224be7a13040a5f8",
+    "url": "/my-song-book/index.html"
+  },
+  {
+    "revision": "c01550ee2dce8b08f3f0",
+    "url": "/my-song-book/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "18f8166c2b92bd35a93c",
+    "url": "/my-song-book/static/js/2.05a2cbe7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-song-book/static/js/2.05a2cbe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01550ee2dce8b08f3f0",
+    "url": "/my-song-book/static/js/main.eb391b30.chunk.js"
+  },
+  {
+    "revision": "16207b7cdd7f19af806a",
+    "url": "/my-song-book/static/js/runtime-main.2fa40b9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-song-book/static/media/logo.5d5d9eef.svg"
+  }
+]);
